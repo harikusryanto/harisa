d5635d2432bf851eeef918f2f1f46c7bfe707f4e
hari kusryanto <hari17ti@mahasiswa.pcr.ac.id>
2018-08-02 23:57:17 +0700

    u

--- arah/index.php
+++ arah/index.php
@@ -24,7 +24,7 @@
 	<div class="navbar-collapse collapse">							
 					<div class="menu">
 						<ul class="nav nav-tabs" role="tablist">
-							<li role="presentation"><a href="../arah/index.html" class="active">Halaman Komentar</a></li>				
+							<li role="presentation"><a href="../admin_komentar/index.php" class="active">Halaman Komentar</a></li>				
 						</ul>
 					</div>
 				</div>		
@@ -68,6 +68,38 @@ $table['rows'] = $rows;
 //mengeluarkan data dengan json_encode. silahkan di echo kalau ingin menampilkan data nya
 $jsonTable = json_encode($table);
 
+
+//Akhir dari table 1 \\
+
+//--------------------------------------------------------------------------------------------------------------------------------------\\
+$rows = array();
+$table = array();
+$table['cols'] = array(
+	//membuat label untuk nama nya, tipe string
+	array('label' => 'Kelas', 'type' => 'string'),
+	//membuat label untuk jumlah siswa, tipe harus number untuk kalkulasi persentasenya
+	array('label' => 'banyak beli', 'type' => 'number')
+);
+
+//melakukan query ke database select
+$sql = $conn->query("SELECT * FROM kelas");
+//perulangan untuk menampilkan data dari database
+while($data = $sql->fetch_assoc()){
+	//membuat array
+	$temp = array();
+	//memasukkan data pertama yaitu nama kelasnya
+	$temp[] = array('v' => (string)$data['kelas']);
+	//memasukkan data kedua yaitu jumlah siswanya
+	$temp[] = array('v' => (int)$data['banyak_beli']);
+	//memasukkan data diatas ke dalam array $rows
+	$rows[] = array('c' => $temp);
+}
+
+//memasukkan array $rows dalam variabel $table
+$table['rows'] = $rows;
+//mengeluarkan data dengan json_encode. silahkan di echo kalau ingin menampilkan data nya
+$jsonTable2 = json_encode($table);
+//Akhir dari table 1 \\
 ?>
 <html>
 <head>
@@ -76,7 +108,7 @@ $jsonTable = json_encode($table);
 
 	google.charts.load('current', {'packages':['corechart']});
 	google.charts.setOnLoadCallback(drawChart);
-
+	google.charts.setOnLoadCallback(drawChart2);
 	function drawChart() {
 
 		// membuat data chart dari json yang sudah ada di atas
@@ -87,7 +119,20 @@ $jsonTable = json_encode($table);
 					   'width':500,
 					   'height':400};
 
-		var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+		var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+		chart.draw(data, options);
+	}
+		function drawChart2() {
+
+		// membuat data chart dari json yang sudah ada di atas
+		var data = new google.visualization.DataTable(<?php echo $jsonTable2; ?>);
+
+		// Set options, bisa anda rubah
+		var options = {'title':'Data Pemelian Paket Wedding Harisa',
+					   'width':500,
+					   'height':400};
+
+		var chart = new google.visualization.LineChart(document.getElementById('chart2_div'));
 		chart.draw(data, options);
 	}
     </script>
@@ -96,6 +141,6 @@ $jsonTable = json_encode($table);
     
 	<!--Div yang akan menampilkan chart-->
     <div id="chart_div"></div>
-	
+	<div id="chart2_div"></div>
 </body>
 </html>
